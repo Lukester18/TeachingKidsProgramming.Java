9603c7876559ad0050c8477bd37b7934db4f452b
Lukester18 <lukeyium@icloud.com>
2016-01-29 12:26:25 -0600

    Can choose multiple endings
    No bugs

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
@@ -48,13 +48,15 @@ public class ChooseYourOwnAdventure
     MessageBox.showMessage(
         "You approach a glowing, green bucket of ooze. Worried that you will get in trouble, you pick up the bucket.");
     //         Ask the user "Do you want to pour the ooze into the 'backyard' or 'toilet'?" --#7
-    String pouringOoze = askAQuestion("Do you want to pour the ooze into the 'backyard' of 'toilet'?");
+    String input = MessageBox.askForTextInput("Do you want to pour the ooze into the 'backyard' or 'toilet'?");
     //         If they answer "toilet" --#8.1
-    if (pouringOoze = toilet)
+    if ("toilet".equalsIgnoreCase(input))
     {
       //            pourIntoToilet (recipe below) --#12.1
       //            ------------- Recipe for pourIntoToilet --#12.2
       //               Tell the user "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste." --#10
+      MessageBox.showMessage(
+          "As you pour the ooze into the toilet it backs up, gurgles, and explodes, covering you in radioactive waste");
       //               Ask the user "Do you want to train to be a NINJA?  'Yes' or 'HECK YES'?" --#11
       //               If they answer "yes" --#13.1
       //                    Tell the user "Awesome dude!  You live out the rest of your life fighting crimes and eating pizza!" --#14
@@ -64,7 +66,7 @@ public class ChooseYourOwnAdventure
       //                    endStory --#16
       //            ------------- End of pourIntoToilet recipe --#12.3
     }
-    else if (pouringOoze = backyard)
+    else if ("backyard".equalsIgnoreCase(input))
     {
       //         Otherwise, if they answer "backyard" --#8.2
       //            pourIntoBackyard (recipe below) --#19.1
@@ -83,6 +85,7 @@ public class ChooseYourOwnAdventure
     else
     {
       //            endStory --#9
+      endStory();
     }
     //      ------------- End of approachOoze recipe --#4.3
   }
