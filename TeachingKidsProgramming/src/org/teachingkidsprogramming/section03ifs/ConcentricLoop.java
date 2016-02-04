9e3bac893e141e7713dde09fe8a557dc61d53621
Lukester18 <lukeyium@icloud.com>
2016-02-04 11:24:19 -0600

    Made loop
    Like a Spiro-Gyro loop.

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
@@ -21,13 +21,21 @@ public class ConcentricLoop
       //      Turn the tortoise 1 degree --#2
       Tortoise.turn(1);
       //      If i is divisible by 20 --(HINT: Use mod operator '%') --#7
-      //          Do the following 360 times --#8.1
-      //              Move the tortoise 1 pixel  --#5  
-      Tortoise.move(1);
-      //              Turn the tortoise 1 degree --#6   
-      Tortoise.turn(1);
-      //              Change the width of the line that the tortoise draws to 1/100th of the current line --#9      
-      //          Repeat --#8.2
+      if (i % 20 == 0)
+      {
+        for (int j = 0; j < 360; j++)
+        {
+          //          Do the following 360 times --#8.1
+          //              Move the tortoise 1 pixel  --#5  
+          Tortoise.move(1);
+          //              Turn the tortoise 1 degree --#6   
+          Tortoise.turn(1);
+          //              Change the width of the line that the tortoise draws to 1/100th of the current line --#9
+          int penWidth = Tortoise.getPenWidth();
+          Tortoise.setPenWidth(penWidth / 100);
+          //          Repeat --#8.2
+        }
+      }
       //   Repeat --#3.2
     }
   }
