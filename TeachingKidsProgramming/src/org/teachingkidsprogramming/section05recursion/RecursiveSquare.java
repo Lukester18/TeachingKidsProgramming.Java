7e4bf8ac6af5dea38eaa08db419371028fa72d17
Lukester18 <lukeyium@icloud.com>
2016-02-19 12:05:44 -0600

    Made a Yellow Square
    Tortoise ends up in the middle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section05recursion/RecursiveSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section05recursion/RecursiveSquare.java
@@ -12,24 +12,37 @@ public class RecursiveSquare
     Tortoise.setPenColor(PenColors.Yellows.Gold);
     //
     //  Set the speed to the fastest --#8
+    Tortoise.setSpeed(10);
     //  Update the length to 100.0 --#1.1
     double length = 100.0;
+    makeASquare(length);
+  }
+  private static void makeASquare(double length)
+  {
     //  MakeASquare with the current length(recipe below) --#11.4
-    //
     //  Create the makeASquare recipe --#11.1
     //  If the current length is greater than 10 --#10.2
-    //      Run the recipe moveToTheSquareStart with the current length  --#4.3
-    //
-    //      Create the moveToTheSquareStart recipe  --#4.1
-    moveToTheSquareStart(length);
-    //
-    //  Do the following 4 times --#7.1
-    //    Move the Tortoise the current length --#6.2
-    Tortoise.move(length);
-    //    MakeASquare with the current length divided by 1.7 (recipe below)--#11.3 
-    //      If the current process count is less than 3 (HINT: use 'i') --#9
-    //          Turn the tortoise 90 degrees to the right --#6.1
-    Tortoise.turn(90);
+    if (length > 10)
+    {
+      //      Run the recipe moveToTheSquareStart with the current length  --#4.3
+      //
+      //      Create the moveToTheSquareStart recipe  --#4.1
+      moveToTheSquareStart(length);
+    }
+    for (int i = 0; i < 4; i++)
+    {
+      //
+      //  Do the following 4 times --#7.1
+      //    Move the Tortoise the current length --#6.2
+      Tortoise.move(length);
+      //    MakeASquare with the current length divided by 1.7 (recipe below)--#11.3
+      //      If the current process count is less than 3 (HINT: use 'i') --#9
+      if (i < 3)
+      {
+        //          Turn the tortoise 90 degrees to the right --#6.1
+        Tortoise.turn(90);
+      }
+    }
     //  Repeat --#7.2
     //
     //  MoveBackToCenter with the current length (recipe below)--#5.3
@@ -37,6 +50,7 @@ public class RecursiveSquare
     moveBackToCenter(length);
     //
     //  Set the current length to the current length times two --#10.1
+    length = length * 2;
     //
     //  End of makeASquare recipe --#11.2
   }
