904cad1744c90dab3e5053a4fce797428faf566e
Lukester18 <lukeyium@icloud.com>
2016-02-29 12:20:23 -0600

    Made First Sentence
    Doesn't work because variable can't be resolved

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
@@ -1,10 +1,14 @@
 package org.teachingkidsprogramming.section06modelviewcontroller;
 
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+
 public class AdLibs
 {
   public static void main(String[] args)
   {
+    String currentStory = "Today I woke " + currentAdverb + ". ";
     //  Ask the user to enter an adverb, save it as currentAdverb --#2
+    String currentAdverb = MessageBox.askForTextInput("Enter an adverb.");
     //  Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
     //  Ask the user to enter a body part, save it as currentBodyPart --#6
     //  Set the value of the currentStory to the word "Today " --#1.2
@@ -12,5 +16,6 @@ public class AdLibs
     //  Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
     //  Add the words "my " + currentBodyPart + ". " to the current story --#7
     //  Show the currentStory in a message box as a message --#1.1
+    MessageBox.showMessage(currentStory);
   }
 }
