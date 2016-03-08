668585c06e7b19d818b35d11a6f668f1504ee99b
Lukester18 <lukeyium@icloud.com>
2016-03-08 12:14:12 -0600

    Did nothing except a few minor changes to editing

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
@@ -12,11 +12,12 @@ public class AdLibs
     String edVerb = MessageBox.askForTextInput("Enter a verd ending in -ed");
     String currentStory = "One day, you decided to walk to the store to buy a " + currentAdverb + ". "
         + "Then you cooked the " + currentAdverb + " for a " + currentBodyPart
-        + " stew. You then ate the stew and " + edVerb + "happily ever after. ";
+        + " stew. You then ate the stew and " + edVerb + " happily ever after. ";
     //  Ask the user to enter an adverb, save it as currentAdverb --#2
     //  Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
     //  Ask the user to enter a body part, save it as currentBodyPart --#6
     //  Set the value of the currentStory to the word "Today " --#1.2
+    // This is a change.mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm 
     //  Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
     //  Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
     //  Add the words "my " + currentBodyPart + ". " to the current story --#7 
