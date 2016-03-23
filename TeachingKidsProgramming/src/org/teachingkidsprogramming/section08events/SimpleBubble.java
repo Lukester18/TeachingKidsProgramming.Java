40fec2b53193e9dbfa3010e5f7c75f244057b01e
Lukester18 <lukeyium@icloud.com>
2016-03-23 11:14:39 -0500

    Made a 5th bubble.
    Program is really cool

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section08events/SimpleBubble.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section08events/SimpleBubble.java
@@ -24,14 +24,16 @@ public class SimpleBubble implements MouseLeftClickListener
   {
     //------------- Recipe for prepareColorPalette --#7
     // Add light steel blue to the color wheel --#4
-    ColorWheel.addColor(PenColors.Browns.SandyBrown);
+    ColorWheel.addColor(PenColors.Blues.LightSteelBlue);
     // Add blue to the color wheel --#5
-    ColorWheel.addColor(PenColors.Grays.Black);
+    ColorWheel.addColor(PenColors.Blues.Blue);
     // Add dark blue to the color wheel --#6
-    ColorWheel.addColor(PenColors.Purples.DarkMagenta);
+    ColorWheel.addColor(PenColors.Blues.DarkBlue);
     // Add purple to the color wheel --#2.3
-    ColorWheel.addColor(PenColors.Blues.Navy);
+    ColorWheel.addColor(PenColors.Purples.Purple);
     //------------- End of prepareColorPalette recipe --#7
+    ColorWheel.addColor(PenColors.Reds.Red);
+    ColorWheel.addColor(PenColors.Whites.White);
   }
   @Override
   public void onLeftMouseClick(int x, int y)
@@ -44,25 +46,29 @@ public class SimpleBubble implements MouseLeftClickListener
     //------------- Recipe for createBubble --#8
     // Remove previous bubbles from your program window --#9
     // Set the radius for the circle to a random number between 10 and 50 --#2.5
-    int radius = NumberUtils.getRandomInt(10, 200);
-    int radius2 = NumberUtils.getRandomInt(10, 150);
-    int radius3 = NumberUtils.getRandomInt(10, 100);
-    int radius4 = NumberUtils.getRandomInt(10, 50);
+    int radius = NumberUtils.getRandomInt(10, 250);
+    int radius2 = NumberUtils.getRandomInt(10, 200);
+    int radius3 = NumberUtils.getRandomInt(10, 150);
+    int radius4 = NumberUtils.getRandomInt(10, 100);
+    int radius5 = NumberUtils.getRandomInt(10, 50);
     // Create a circle with the radius and the next color from the color wheel --#2.1
     Circle circle = new Circle(radius, ColorWheel.getRandomColorFromWheel());
     Circle circle2 = new Circle(radius2, ColorWheel.getRandomColorFromWheel());
     Circle circle3 = new Circle(radius3, ColorWheel.getRandomColorFromWheel());
     Circle circle4 = new Circle(radius4, ColorWheel.getRandomColorFromWheel());
+    Circle circle5 = new Circle(radius5, ColorWheel.getRandomColorFromWheel());
     // Move the center of the bubble to the current position of the mouse on the window --#3
     circle.setCenter(x, y);
     circle2.setCenter(x + 0, y);
     circle3.setCenter(x, y + 0);
     circle4.setCenter(x + 0, y + 0);
+    circle5.setCenter(x - 450, y - 450);
     // Add the circle to your program window --#2.4
     circle.addTo(programWindow);
     circle2.addTo(programWindow);
     circle3.addTo(programWindow);
     circle4.addTo(programWindow);
+    circle5.addTo(programWindow);
     //------------- End of createBubble recipe --#8
   }
   public static void main(String[] args)
