a167c6235b66017b3c2a864ed6cc3ac655eb9615
Lukester18 <lukeyium@icloud.com>
2016-03-23 13:03:50 -0500

    Started Rectanglekata.
    Tortoise makes a square. I also translated the code into English

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/RectangleKata.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/RectangleKata.java
@@ -1,10 +1,14 @@
 package org.teachingkidsprogramming.section09final;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Grays;
+
 public class RectangleKata
 {
   public static void main(String[] args)
   {
     //    Draw a rectangle
+    Tortoise.drawShape(4, Grays.Black, 80, 5);
     //    Write the steps to code your rectangle out in English 
     //    Translate from English to Java one line at a time
     //    Run your code after each line of Java to make sure it works as expected
