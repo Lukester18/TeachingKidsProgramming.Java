c62836cd6e383fcc7ff53a1570b1a3a1a5b13bda
Lukester18 <lukeyium@icloud.com>
2016-03-28 13:02:57 -0500

    Made 4 more circles
    now there is 9

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section08events/SimpleBubble.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section08events/SimpleBubble.java
@@ -43,30 +43,47 @@ public class SimpleBubble implements MouseLeftClickListener
   {
     //------------- Recipe for createBubble --#8
     // Remove previous bubbles from your program window --#9
+    programWindow.clearWindow();
     // Set the radius for the circle to a random number between 10 and 50 --#2.5
-    int radius = NumberUtils.getRandomInt(10, 250);
-    int radius2 = NumberUtils.getRandomInt(10, 200);
-    int radius3 = NumberUtils.getRandomInt(10, 150);
+    int radius = NumberUtils.getRandomInt(10, 100);
+    int radius2 = NumberUtils.getRandomInt(10, 100);
+    int radius3 = NumberUtils.getRandomInt(10, 100);
     int radius4 = NumberUtils.getRandomInt(10, 100);
-    int radius5 = NumberUtils.getRandomInt(10, 50);
+    int radius5 = NumberUtils.getRandomInt(10, 100);
+    int radius6 = NumberUtils.getRandomInt(10, 100);
+    int radius7 = NumberUtils.getRandomInt(10, 100);
+    int radius8 = NumberUtils.getRandomInt(10, 100);
+    int radius9 = NumberUtils.getRandomInt(10, 100);
     // Create a circle with the radius and the next color from the color wheel --#2.1
     Circle circle = new Circle(radius, ColorWheel.getRandomColorFromWheel());
     Circle circle2 = new Circle(radius2, ColorWheel.getRandomColorFromWheel());
     Circle circle3 = new Circle(radius3, ColorWheel.getRandomColorFromWheel());
     Circle circle4 = new Circle(radius4, ColorWheel.getRandomColorFromWheel());
     Circle circle5 = new Circle(radius5, ColorWheel.getRandomColorFromWheel());
+    Circle circle6 = new Circle(radius6, ColorWheel.getRandomColorFromWheel());
+    Circle circle7 = new Circle(radius7, ColorWheel.getRandomColorFromWheel());
+    Circle circle8 = new Circle(radius8, ColorWheel.getRandomColorFromWheel());
+    Circle circle9 = new Circle(radius9, ColorWheel.getRandomColorFromWheel());
     // Move the center of the bubble to the current position of the mouse on the window --#3
     circle.setCenter(x, y);
-    circle2.setCenter(x + 0, y);
-    circle3.setCenter(x, y + 0);
-    circle4.setCenter(x + 0, y + 0);
-    circle5.setCenter(x - 0, y - 0);
+    circle2.setCenter(x + 100, y);
+    circle3.setCenter(x, y + 100);
+    circle4.setCenter(x - 100, y);
+    circle5.setCenter(x, y - 100);
+    circle6.setCenter(x + 100, y + 100);
+    circle7.setCenter(x - 100, y + 100);
+    circle8.setCenter(x + 100, y - 100);
+    circle9.setCenter(x - 100, y - 100);
     // Add the circle to your program window --#2.4
     circle.addTo(programWindow);
     circle2.addTo(programWindow);
     circle3.addTo(programWindow);
     circle4.addTo(programWindow);
     circle5.addTo(programWindow);
+    circle6.addTo(programWindow);
+    circle7.addTo(programWindow);
+    circle8.addTo(programWindow);
+    circle9.addTo(programWindow);
     //------------- End of createBubble recipe --#8
   }
   public static void main(String[] args)
