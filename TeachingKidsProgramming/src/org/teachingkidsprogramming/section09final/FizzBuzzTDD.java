f16182914a6351a5f83bfd8bb5a2d18c6cab5a2d
Lukester18 <lukeyium@icloud.com>
2016-03-28 12:32:38 -0500

    Made some tests for FizzBuzzTdd
    Don't know what happened with regular fizzbuzz

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
@@ -1,14 +1,37 @@
 package org.teachingkidsprogramming.section09final;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class FizzBuzzTDD
 {
-  //    For the numbers being tested, print out either that number, or, 
-  //    If that number is evenly divisible by 3, then print the word 'Fizz', 
-  //    If that number is evenly divisible by 5, then print the word 'Buzz', 
-  //    If that number is evenly divisible by either 3 or 5, then print the word 'FizzBuzz'
-  //
-  //    Write tests using the Assert object via the TDD style
-  //
-  //    For more complete directions see this page
-  //    https://www.penflip.com/lynnlangit/tkp-lesson-plans/blob/master/course09.txt
+  public static String convert(int i)
+  {
+    //    For the numbers being tested, print out either that number, or, 
+    //    If that number is evenly divisible by 3, then print the word 'Fizz', 
+    if (0 == i % 3) { return "Fizz"; }
+    //    If that number is evenly divisible by 5, then print the word 'Buzz', 
+    if (0 == i % 5) { return "Buzz"; }
+    //    If that number is evenly divisible by either 3 or 5, then print the word 'FizzBuzz'
+    if (0 == i % 15) { return "FizzBuzz"; }
+    return "" + i;
+    //
+    //    Write tests using the Assert object via the TDD style
+    //
+    //    For more complete directions see this page
+    //    https://www.penflip.com/lynnlangit/tkp-lesson-plans/blob/master/course09.txt
+  }
+  @Test
+  public void WhatAbout20()
+  {
+    String result = FizzBuzzTDD.convert(20);
+    assertEquals("Buzz", result);
+  }
+  @Test
+  public void YourFace()
+  {
+    String result = FizzBuzzTDD.convert(12);
+    assertEquals("Fizz", result);
+  }
 }
