73723ceca6d8eba2d3b5ff5379152d8afe169b03
Lukester18 <lukeyium@icloud.com>
2016-03-28 11:03:25 -0500

    Completed RectangleKata
    Might make a variation, we'll see.

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/RectangleKata.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/RectangleKata.java
@@ -10,7 +10,17 @@ public class RectangleKata
     //    Draw a rectangle
     Tortoise.drawShape(4, Grays.Black, 80, 5);
     //    Write the steps to code your rectangle out in English 
+    //    First, show the tortoise. Then, set the pen width to 5. After that, make a for loop, and change args.length to 4.
+    //    Next, inside the brackets, make the tortoise move 80 pixels. Then, make the tortoise turn 90 degrees to the 
+    //    right. Now you are done!
     //    Translate from English to Java one line at a time
+    Tortoise.show();
+    Tortoise.setPenWidth(5);
+    for (int i = 0; i < 4; i++)
+    {
+      Tortoise.move(80);
+      Tortoise.turn(90);
+    }
     //    Run your code after each line of Java to make sure it works as expected
   }
 }
