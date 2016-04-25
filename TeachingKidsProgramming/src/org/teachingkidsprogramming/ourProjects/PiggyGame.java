5466d8de17d7d32c859b1ad37d5a3635d57ebf38
Lukester18 <lukeyium@icloud.com>
2016-04-25 13:15:21 -0500

    Gave up
    Working on coding bat

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/ourProjects/PiggyGame.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/ourProjects/PiggyGame.java
@@ -7,15 +7,14 @@ public class PiggyGame
 {
   public static void main(String[] args)
   {
-    String firstRollPlayer1 = MessageBox.askForTextInput("Do you wish to 'Roll' the dice or 'Hold' your score?");
-    if ("Roll".equalsIgnoreCase(firstRollPlayer1))
+    String firstRollHold1 = MessageBox.askForTextInput("Do you wish to 'Roll' the dice or 'Hold' your score?");
+    if ("Roll".equalsIgnoreCase(firstRollHold1))
     {
       int diceRoll = NumberUtils.getRandomInt(1, 6);
       MessageBox.showMessage("You rolled a " + diceRoll + ".");
-      int player1StartingScore = diceRoll;
-      int player1NewScore = player1StartingScore;
-      int player1Current = player1NewScore;
-      MessageBox.showMessage("Your score is " + player1NewScore + ".");
+      int firstScore1 = diceRoll;
+      int newScore1 = diceRoll;
+      MessageBox.showMessage("Your score is " + newScore1);
     }
   }
 }
